3ef5ea521e223e4313d2b2afd8b2361f50cdb705
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-08-31 19:13:36 +0200

    WIP

--- Sources/CreateNotes.swift
+++ Sources/CreateNotes.swift
@@ -29,6 +29,35 @@ struct CreateNotes: AsyncParsableCommand {
 	@Argument(completion: .directory, transform: URL.init(fileURLWithPath:))
 	var mediaDirectory: URL
 
+	func createSilence(filePath: String) throws {
+		let silenceCmd = "ffmpeg -f lavfi -i anullsrc=r=16000:cl=mono -t \(0.5) \(filePath)"
+		let process = Process()
+		process.launchPath = "/usr/bin/env"
+		process.arguments = ["bash", "-c", silenceCmd]
+		process.launch()
+		process.waitUntilExit()
+	}
+
+	func concatenateAudioFiles(inputFilePaths: [String], outputFileURL finalOutputFileURL: URL) throws {
+		let inputFilePathsFileURL = URL.currentDirectory().appending(path: "input-file-paths.txt")
+		let inputFilePathsFilePath = inputFilePathsFileURL.path()
+		let inputFilePathsFileContent = inputFilePaths.map { "file '\($0)'" }.joined(separator: "\n")
+		try inputFilePathsFileContent.write(toFile: inputFilePathsFilePath, atomically: true, encoding: .utf8)
+		let temporaryOutputFileURL = URL.currentDirectory().appending(path: "output.mp3", directoryHint: .notDirectory)
+		let process = Process()
+		process.launchPath = "/usr/bin/env"
+		process.arguments = [
+			"bash",
+			"-c",
+			"ffmpeg -f concat -safe 0 -i \(inputFilePathsFilePath) -c copy \(temporaryOutputFileURL.path)"
+		]
+		process.launch()
+		process.waitUntilExit()
+
+		try FileManager.default.moveItem(at: temporaryOutputFileURL, to: finalOutputFileURL)
+		try FileManager.default.removeItem(at: inputFilePathsFileURL)
+	}
+
 	func downloadMissingAudioFiles(
 		minimalPairComponents: [MinimalPairComponent],
 		exportDirectory: URL
@@ -68,19 +97,38 @@ struct CreateNotes: AsyncParsableCommand {
 			}
 		}
 
-
+		let silenceFileURL = URL.currentDirectory().appending(path: "silence.wav")
+		let silenceFilePath = silenceFileURL.path()
+		try createSilence(filePath: silenceFilePath)
 		for (filename, ttsResponse) in ttsReponses {
-			try await Task.sleep(for: .seconds(5))
+			try await Task.sleep(for: .seconds(1))
 			let (localURL, response2) = try await URLSession.shared.download(from: ttsResponse.async)
 			guard let httpResponse = response2 as? HTTPURLResponse,
 					(200...299).contains(httpResponse.statusCode) else {
 				print("Error getting \(filename)")
 				continue
 			}
-			print(localURL)
-			try FileManager.default.moveItem(at: localURL, to: exportDirectory.appending(component: filename))
+			let femaleWavPath = localURL.path()
+			let maleWavPath = localURL.path()
+
+			let inputFilePaths = [
+				femaleWavPath,
+				silenceFilePath,
+				maleWavPath,
+				silenceFilePath,
+				femaleWavPath,
+				silenceFilePath,
+				maleWavPath
+			]
+
+			try concatenateAudioFiles(
+				inputFilePaths: inputFilePaths,
+				outputFileURL:  exportDirectory.appending(component: filename)
+			)
 		}
+		try FileManager.default.removeItem(at: silenceFileURL)
 	}
+
 	func exportCSVFile(
 		minimalPairComponents: [MinimalPairComponent],
 		exportDirectory: URL

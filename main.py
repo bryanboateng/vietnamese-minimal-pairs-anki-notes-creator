431e688aee51e7a05902bd7d88906b326c521d5b
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-09-23 00:58:36 +0200

    Process multiple texts

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import argparse
 from io import BytesIO
 from pathlib import Path
 
 from google.cloud import texttospeech
 from pydub import AudioSegment
+from tqdm import tqdm
 
 
 def synthesize_repeated_text(
@@ -26,40 +26,42 @@ def synthesize_repeated_text(
     return response.audio_content
 
 
-def text_to_speech(text: str):
+def text_to_speech(texts: list[str]):
     client = texttospeech.TextToSpeechClient()
-    filename = f"tts-{text}.wav"
+    for text in tqdm(texts, unit="text"):
+        out_directory_path = Path("./out")
+        file_path = out_directory_path / f"tts-{text}.wav"
 
-    female_audio = AudioSegment.from_wav(
-        BytesIO(
-            synthesize_repeated_text(
-                text=text, client=client, voice_name="vi-VN-Neural2-A"
+        if file_path.exists():
+            print(f'"{text}" already exists, skipping')
+        else:
+            print(f'Doing "{text}"...')
+            female_audio = AudioSegment.from_wav(
+                BytesIO(
+                    synthesize_repeated_text(
+                        text=text, client=client, voice_name="vi-VN-Neural2-A"
+                    )
+                )
             )
-        )
-    )
-    male_audio = AudioSegment.from_wav(
-        BytesIO(
-            synthesize_repeated_text(
-                text=text, client=client, voice_name="vi-VN-Neural2-D"
+            male_audio = AudioSegment.from_wav(
+                BytesIO(
+                    synthesize_repeated_text(
+                        text=text, client=client, voice_name="vi-VN-Neural2-D"
+                    )
+                )
             )
-        )
-    )
 
-    silence_duration_ms = 500
-    silence = AudioSegment.silent(duration=silence_duration_ms)
-    combined_audio = female_audio + silence + male_audio
-    out_directory_path = Path("./out")
-    out_directory_path.mkdir(parents=True, exist_ok=True)
-    combined_audio.export(out_directory_path / filename, format="wav")
+            silence_duration_ms = 500
+            silence = AudioSegment.silent(duration=silence_duration_ms)
+            combined_audio = female_audio + silence + male_audio
+            out_directory_path.mkdir(parents=True, exist_ok=True)
+            combined_audio.export(file_path, format="wav")
+            print(f'Exported "{text}".')
 
 
 def main():
-    argument_parser = argparse.ArgumentParser(
-        prog="Vietnamese Minimal Pairs Audio Creator"
-    )
-    argument_parser.add_argument("text", type=str)
-    args = argument_parser.parse_args()
-    text_to_speech(text=args.text)
+    texts = Path("in.txt").read_text(encoding="utf-8").splitlines()
+    text_to_speech(texts=texts)
 
 
 if __name__ == "__main__":
